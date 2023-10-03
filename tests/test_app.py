d26cf1e919b47ba549d177c58b78ca06303cb2cb
Mohammed Achraf Lebrazi <lebrazi.med.achraf@gmail.com>
2023-10-04 00:58:07 +0200

    Added some tests to run on CI

---
+++ tests/test_app.py
@@ -0,0 +1,36 @@
+import pytest
+from ..app import app
+from flask import session
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_index_route(client):
+    response = client.get('/')
+    assert b'Welcome to Flaskpedia!' in response.data
+
+def test_about_route(client):
+    response = client.get('/about')
+    assert b'About' in response.data
+
+def test_search_route(client):
+    response = client.get('/search')
+    assert b'Search' in response.data
+
+def test_result_route(client):
+    response = client.get('/result')
+    assert b'No keyword provided.' in response.data
+
+# FAILS
+def test_save_bookmark_route(client):
+    response = client.post('/bookmark/Morocco', follow_redirects=True)
+    assert b'Page "Morocco" bookmarked successfully!' in response.data
+
+def test_clear_bookmark_route(client):
+    client.get('/clear_bookmark', follow_redirects=True)
+    assert session['bookmarks'] == []
+
+
